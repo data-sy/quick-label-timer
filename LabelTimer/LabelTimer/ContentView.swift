b8d02086180bfc6e2b7c2108b691bd879f8f6197
Lee Soyeon <data.sy.2@gmail.com>
2025-07-06 20:02:55 +0900

    chore: create initial project setup

---
+++ LabelTimer/LabelTimer/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  LabelTimer
+//
+//  Created by 이소연 on 7/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
---
+++ LabelTimer/LabelTimer/LabelTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabelTimerApp.swift
+//  LabelTimer
+//
+//  Created by 이소연 on 7/6/25.
+//
+
+import SwiftUI
+
+@main
+struct LabelTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
