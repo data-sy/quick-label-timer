dcb308cdc324978742a5d4e63eae666481c3a6e3
Lee Soyeon <data.sy.2@gmail.com>
2025-08-30 20:50:10 +0900

    feat: add centralized Logger extension
    - Add Logger+Extension to create loggers consistently
    - Use dynamic bundle identifier for subsystem to avoid hardcoding

---
+++ QuickLabelTimer/QuickLabelTimer/Utils/Logger+Extension.swift
@@ -0,0 +1,23 @@
+//
+//  Logger+Extension.swift
+//  QuickLabelTimer
+//
+//  Created by 이소연 on 8/30/25.
+//
+/// OSLog.Logger를 앱 전역에서 일관된 방식으로 생성하고 사용하기 위한 확장 파일
+///
+/// - 사용 목적:
+///   - 모든 Logger 인스턴스가 동적으로 가져온 번들 ID를 subsystem으로 공유하도록 보장
+///   - 각기 다른 파일(category)에서 Logger를 간편하게 생성
+///   - 로거 초기화와 관련된 코드 중복 방지 및 하드코딩 제거
+
+import Foundation
+import OSLog
+
+extension Logger {
+    private static var subsystem = Bundle.main.bundleIdentifier!
+
+    static func withCategory(_ category: String) -> Logger {
+        return Logger(subsystem: subsystem, category: category)
+    }
+}
