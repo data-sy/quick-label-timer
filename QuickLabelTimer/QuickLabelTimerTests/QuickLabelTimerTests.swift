20ff3a0cafae9d9e2e6dc8e78d023d370a8cfac0
Lee Soyeon <data.sy.2@gmail.com>
2025-09-02 01:18:22 +0900

    chore: remove outdated unit tests
    - Remove all existing test files from the test target
    - Current tests target deprecated models (TimerManager) and outdated logic
    - Pave the way for new, relevant tests for the TimerService

--- QuickLabelTimer/QuickLabelTimerTests/QuickLabelTimerTests.swift
+++ QuickLabelTimer/QuickLabelTimerTests/QuickLabelTimerTests.swift
@@ -1,5 +1,5 @@
 //
-//  LabelTimerTests.swift
+//  QuickLabelTimerTests.swift
 //  QuickLabelTimer
 //
 //  Created by 이소연 on 7/9/25.
@@ -7,7 +7,7 @@
 
 import Testing
 
-struct LabelTimerTests {
+struct QuickLabelTimerTests {
 
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
