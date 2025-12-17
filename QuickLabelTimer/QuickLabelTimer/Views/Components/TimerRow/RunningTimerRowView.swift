598dd496c607538e80c3fa91ee2dd9aa056f8fb0
Lee Soyeon <data.sy.2@gmail.com>
2025-12-17 20:54:36 +0900

    feat: migrate RunningTimersView to NewTimerRow
    - Replace TimerRowView with NewTimerRow in running timers list
    - Map existing left and right tap handlers to play pause reset and delete actions
    - Ensure proper view refresh on deletion using stable id

--- QuickLabelTimer/QuickLabelTimer/Views/Components/TimerRow/RunningTimerRowView.swift
+++ QuickLabelTimer/QuickLabelTimer/Views/Components/TimerRow/RunningTimerRowView.swift
@@ -17,18 +17,13 @@ struct RunningTimerRowView: View {
     let onRightTap: (() -> Void)?
 
     var body: some View {
-        let statusText = self.statusText(for: timer.interactionState)
-
-        VStack(alignment: .leading, spacing: 4) {
-            TimerRowView(
-                timer: timer,
-                state: timer.interactionState,
-                statusText: statusText,
-                onToggleFavorite: onToggleFavorite,
-                onLeftTap: onLeftTap,
-                onRightTap: onRightTap
-            )
-        }
+        NewTimerRow(
+            timer: timer,
+            onToggleFavorite: onToggleFavorite ?? {},
+            onPlayPause: onRightTap ?? {},
+            onReset: onLeftTap ?? {},
+            onDelete: onLeftTap ?? {}
+        )
         .id(timer.id) // 삭제 후 뷰 리프레시 보장
     }
     
